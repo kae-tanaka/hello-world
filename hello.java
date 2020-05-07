b9e6e06405a767f310b30bdbd871248fe847cbea
k-tanaka <kae.tanaka@acroit.co.jp>
2020-05-07 16:07:34 +0900

    testcommit

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class kensyu {
+	public static void main(String[] args) {
+		int score = 71;
+		//String judgment;
+		if (score >= 80) {
+			System.out.println("優");
+		} else if (score >= 50) {
+			System.out.println("良");
+		} else if (score >= 30) {
+			System.out.println("可");
+		} else {
+			System.out.println("不合格");
+		}
+		//System.out.println("指定された数値は『" + judgment + "』判定です");
+	}
+}
+
